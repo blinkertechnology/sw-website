d6024d4b7bc9c18be9bad97c9f31bdea431b2ca9
manu-xo5 <mohit.matwaya@gmail.com>
2024-03-05 20:54:26 +0530

    feat: stats section

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -1,7 +1,46 @@
+import bg from "@/assets/stats.jpeg";
+
 export const StatsSection: React.FC = () => {
   return (
-    <div>
-      
+    <div
+      style={{ backgroundImage: `url(${bg.src})` }}
+      className="aspect-[2] mx-11 mt-[198px] rounded-[120px] bg-no-repeat bg-cover"
+    >
+      <p className="pt-[136px] font-semibold text-[22px] text-center tracking-[31%] text-[rgba(228,228,228,0.28)]">
+        OUR NUMBERS
+      </p>
+
+      <div className="max-w-[1400px] mx-auto flex justify-between pt-[84px]">
+        <p>
+          <span className="font-bold text-[118px] text-white leading-[104px] tracking-[-4%] ">
+            60,000+
+          </span>
+          <br />
+          <span className="text-white/70 text-[34px] leading-[60px] font-medium">
+            Downloads since March 2023
+          </span>
+        </p>
+
+        <p>
+          <span className="font-bold text-[118px] text-white leading-[104px] tracking-[-4%] ">
+            64
+          </span>
+          <br />
+          <span className="text-white/70 text-[34px] leading-[60px] font-medium">
+            Countries
+          </span>
+        </p>
+
+        <p>
+          <span className="font-bold text-[118px] text-white leading-[104px] tracking-[-4%] ">
+            400M+
+          </span>
+          <br />
+          <span className="text-white/70 text-[34px] leading-[60px] font-medium">
+            Total Addressable Market
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
