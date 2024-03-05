3729814e56eb6ca06deab2f3a20de94bdc893617
manu-xo5 <mohit.matwaya@gmail.com>
2024-03-05 20:59:56 +0530

    feat: add 700 font weight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 
-const custom = Poppins({ weight: ["400", "500", "600"], subsets: ["latin"] });
+const custom = Poppins({ weight: ["400", "500", "600", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sorted Wallet",
