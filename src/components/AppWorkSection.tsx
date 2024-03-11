cba71cd932f3633f17cefe4fc597eb97ca60266f
manu-xo5 <mohit.matwaya@gmail.com>
2024-03-11 22:26:08 +0530

    feat: app working mobile version

--- src/components/AppWorkSection.tsx
+++ src/components/AppWorkSection.tsx
@@ -9,7 +9,7 @@ import { PlayFillIcon } from "./icons/play-fill-icon";
 export const AppWorkSection: React.FC = () => {
   return (
     <div className="pt-64 xl:px-6 desktop:px-10 relative">
-      <div className="font-semibold text-5xl desktop:text-8xl text-center leading-[1] text-primary-foreground">
+      <div className="font-semibold text-4xl xl:text-5xl desktop:text-8xl text-center leading-[1] text-primary-foreground">
         <p>
           How{" "}
           <span className="relative">
@@ -19,12 +19,73 @@ export const AppWorkSection: React.FC = () => {
             </span>
           </span>{" "}
         </p>
-        <p>works</p>
+        <p>as an app works</p>
       </div>
 
-      <p className="text-left pt-6 desktop:pt-20 max-w-lg mx-auto font-semibold text-2xl desktop:text-center text-primary-foreground">
+      <p className="hidden xl:block text-center pt-6 desktop:pt-20 max-w-lg mx-auto font-semibold xl:text-2xl desktop:text-center text-primary-foreground">
         Implements robust security measures to protect user funds and data.
       </p>
+
+      <div className="mx-auto max-w-xl xl:hidden pt-16 px-4">
+        <div className="grid grid-cols-[auto,1fr] gap-4">
+          <div className="h-full w-min flex flex-col items-center gap-2 pt-1">
+            <span className="flex-none inline-block size-3.5 bg-gray-800 rounded-full" />
+
+            <span className="flex-1 w-px border border-dashed" />
+          </div>
+
+          <div className="pb-4">
+            <p className="text-primary-foreground font-semibold">
+              Register for Sorted Wallet
+            </p>
+
+            <p className="text-secondary-foreground">
+              Provides crypto services to feature phone users in emerging
+              markets Provides crypto services to feature.
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-[auto,1fr] gap-4">
+          <div className="h-full w-min flex flex-col items-center gap-2 pt-1">
+            <span className="flex-none inline-block size-3.5 bg-gray-800 rounded-full" />
+
+            <span className="flex-1 w-px border border-dashed" />
+          </div>
+
+          <div className="pb-4">
+            <p className="text-primary-foreground font-semibold">
+              Seamlessly store, send, and receive digital assets
+            </p>
+
+            <p className="text-secondary-foreground">
+              Provides crypto services to feature phone users in emerging
+              markets Provides crypto services to feature.
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-[auto,1fr] gap-4">
+          <div className="h-full w-min flex flex-col items-center gap-2 pt-1">
+            <span className="flex-none inline-block size-3.5 bg-gray-800 rounded-full" />
+          </div>
+
+          <div className="pb-4">
+            <p className="text-primary-foreground font-semibold">
+              Manage your wallets and explore all features
+            </p>
+
+            <p className="text-secondary-foreground">
+              Provides crypto services to feature phone users in emerging
+              markets Provides crypto services to feature.
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-accent-blue/[0.05] rounded-3xl px-8 py-12 text-xl desktop:text-2xl flex items-center text-primary-foreground font-semibold">
+          Provides crypto services to feature phone users in emerging markets
+        </div>
+      </div>
 
       <div className="hidden xl:grid grid-cols-5 h-[430px] gap-x-4 desktop:gap-x-8">
         {/* card 1 */}
@@ -95,7 +156,9 @@ export const AppWorkSection: React.FC = () => {
 
           <div className="flex items-center gap-2 desktop:gap-4 bg-black/50 -mx-2 desktop:-mx-0 p-2 desktop:p-4 rounded-lg desktop:rounded-2xl">
             <PlayFillIcon className="flex-none box-content p-2 size-4 fill-black bg-white rounded-full" />
-            <p className="text-sm desktop:text-base text-white font-semibold">Watch how it works.</p>
+            <p className="text-sm desktop:text-base text-white font-semibold">
+              Watch how it works.
+            </p>
           </div>
         </div>
       </div>
